90bbb2a30c85a5a4549595994eba5d0f52b90757
pannichhhh <panichakam.k@gmail.com>
2024-09-03 14:56:15 +0700

    conncet db

--- src/app/client-server-component/action.js
+++ src/app/client-server-component/action.js
@@ -1,5 +1,33 @@
 'use server'
 
-export async function submitForm() {
-    
+import mysql from 'mysql2'
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'panichakam',
+    database:'dbproducts',
+    port:'3306'
+})
+
+export async function submitForm(formData) {
+    // recieve data : id , customer_name , address , phone
+    const id = formData.get('id')
+    const customer_name = formData.get('customer_name')
+    const address = formData.get('address')
+    const phone = formData.get('phone')
+
+    console.log('id',id)
+    console.log('customer_name',customer_name)
+    console.log('address',address)
+    console.log('phone',phone)
+
+    const sql = `INSERT INTO ecommerce (customer_id, customer_name, address, phone) VALUES (?,?,?,?)`
+    const values = [id,customer_name,address,phone]
+
+    connection.query(sql,values,(error,results,fields) => {
+        if(error){
+            return console.error(error.message)
+        }
+    })
 }
